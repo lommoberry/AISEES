82887cd15e94aa8fdb4afb6448ce652d9f217ad2
Bridget Kim <Bridget Kim>
2024-04-08 16:42:47 -0400

    updated security

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,28 +12,30 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# Load environment variables from '.env'
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)8+g=n99k=&dlark94ihos0q4m)c4(!5vs6!yn^m*8p!!7h%_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['xpt3bn.pythonanywhere.com']
+ALLOWED_HOSTS = ['xpt3bn.pythonanywhere.com','127.0.0.1']
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "bridgetkim02@gmail.com"
-EMAIL_HOST_PASSWORD = "kddn gvng rxny ihio"
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Delight youremail@gmail.com"
