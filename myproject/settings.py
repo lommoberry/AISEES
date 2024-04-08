01ddf803d05cf84dc7718fbfb45b508c62d60c59
Bridget Kim <Bridget Kim>
2024-04-08 18:06:09 -0400

    confused

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv # type: ignore
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Load environment variables from '.env'
-load_dotenv()
+load_dotenv(os.path.join(BASE_DIR,'.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
